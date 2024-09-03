c9831e8282d40d7be0d47ebc6f6fb39a9f3dbd9b
Xlucidator <rubixue572@gmail.com>
2024-09-03 13:59:13 +0800

    check bat scripts, Ctrl+C trap unabled

--- rsrc_update.bat
+++ rsrc_update.bat
@@ -1,18 +1,46 @@
-@REM @echo off
+@echo off
 setlocal
 
 set "skip_download=false"
 
 set "rsrc_url=https://github.com/MaaAssistantArknights/MaaResource/archive/refs/heads/main.zip"
-@REM set "maa_dir=MAA-v5.5.0-beta.1-win-x64"
-set "maa_dir=test"
+set "maa_dir=MAA-v5.5.0-beta.1-win-x64"
 set "maarsrc_dir=MaaResource-main"
 set "zip_file=%maarsrc_dir%.zip"
+
+@REM React to Ctrl+C interrupt, but seems useless
+@REM for /f "tokens=2 delims==" %%i in ('"exit /b 0"') do set "trap=call :%trap_handler%" > nul 2>&1
 
 if not exist "%maa_dir%" (
     echo [error] Target MAA directory does not exist
     exit /b 1
 )
+
+for %%i in (%*) do (
+    if "%%i"=="-s" (
+        set "skip_download=true"
+    ) else if "%%i"=="-h" (
+        echo Usage: %0 [-s|--skip-download] [-h|--help]
+        echo   -s, --skip-download    Skip the download operation
+        echo   -h, --help             Display help information
+        exit /b 0
+    ) else (
+        echo Invalid argument: %%i
+        exit /b 1
+    )
+)
+
+if "%skip_download%"=="true" (
+    echo Skipping download of zip file
+) else (
+    call :download_file
+)
+call :unzip_file
+call :process_file
+call :clean_up
+exit /b 0
+
+@REM /* Definition of Functions */
 
 :clean_up
 echo Cleaning up
@@ -45,35 +73,12 @@ goto :eof
 
 :process_file
 echo Copying cache/ and resource/ to %maa_dir%
-xcopy /s /e "%maarsrc_dir%\cache" "%maa_dir%\cache"
-xcopy /s /e "%maarsrc_dir%\resource" "%maa_dir%\resource"
+xcopy /s /e /y "%maarsrc_dir%\cache" "%maa_dir%\cache\"
+xcopy /s /e /y "%maarsrc_dir%\resource" "%maa_dir%\resource\"
 echo Finished.
 goto :eof
 
-:trap
+:trap_handler
 call :clean_up
 exit /b 1
 
-for %%i in (%*) do (
-    if "%%i"=="-s" (
-        set "skip_download=true"
-    ) else if "%%i"=="-h" (
-        echo Usage: %0 [-s|--skip-download] [-h|--help]
-        echo   -s, --skip-download    Skip the download operation
-        echo   -h, --help             Display help information
-        exit /b 0
-    ) else (
-        echo Invalid argument: %%i
-        exit /b 1
-    )
-)
-
-if "%skip_download%"=="true" (
-    echo Skipping download of zip file
-) else (
-    call :download_file
-)
-
-call :unzip_file
-call :process_file
-call :clean_up
